7c08275d76ff6998f6f30404de58a7364607f921
Prajakta Pohare <prajaktapohare73@gmail.com>
2025-03-02 13:40:18 -0600

    Variables, expressions and statements

---
+++ week-2/Python2_1.py
@@ -0,0 +1,7 @@
+Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 5
+5
+>>> x = 5
+>>> x + 1
+6
---
+++ week-2/Python2_2.py
@@ -0,0 +1,15 @@
+Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> width = 17
+>>> height = 12.0
+>>> delimiter = '.'
+>>> print(width / 2, type(width / 2))
+8.5 <class 'float'>
+>>> print(width / 2.0, type(width / 2.0))
+8.5 <class 'float'>
+>>> print(height / 3, type(height / 3))
+4.0 <class 'float'>
+>>> print(1 + 2 * 5, type(1 + 2 * 5))
+11 <class 'int'>
+>>> print(delimiter * 5, type(delimiter * 5))
+..... <class 'str'>
